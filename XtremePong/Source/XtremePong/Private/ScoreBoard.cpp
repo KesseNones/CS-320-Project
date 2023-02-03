dcab0f389a0bae531930842b4a63b526765fc51a
KesseNones <jesse.jones@wsu.edu>
2023-02-03 15:23:40 -0800

    Added Scoreboard Class
    The scoreboard class skeleton was added to the project.
    Presently it's empty.

---
+++ XtremePong/Source/XtremePong/Private/ScoreBoard.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ScoreBoard.h"
+
+// Sets default values
+AScoreBoard::AScoreBoard()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AScoreBoard::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AScoreBoard::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
