156d7dc95821749701904aacdb7bb709f87a3107
Dean <dnfeller07@gmail.com>
2023-02-05 16:51:16 -0800

    Add basic main menu

--- XtremePong/Source/XtremePong/XtremePongGameModeBase.cpp
+++ XtremePong/Source/XtremePong/XtremePongGameModeBase.cpp
@@ -3,3 +3,24 @@
 
 #include "XtremePongGameModeBase.h"
 
+void AXtremePongGameModeBase::BeginPlay() {
+	Super::BeginPlay();
+	ChangeMenuWidget(StartingWidgetClass);
+}
+
+void AXtremePongGameModeBase::ChangeMenuWidget(TSubclassOf<UUserWidget> NewWidgetClass) {
+
+	// Remove whatever User Widget is active in the viewport, if any
+	if (CurrentWidget != nullptr) {
+		CurrentWidget->RemoveFromViewport();
+		CurrentWidget = nullptr;
+	}
+
+	// Create a new User Widget and add it to the viewport
+	if (NewWidgetClass != nullptr) {
+		CurrentWidget = CreateWidget<UUserWidget>(GetWorld(), NewWidgetClass);
+		if (CurrentWidget != nullptr) {
+			CurrentWidget->AddToViewport();
+		}
+	}
+}
