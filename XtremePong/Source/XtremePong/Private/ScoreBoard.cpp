268b6cd6d92d499322f1ad2e5fba7eb04082683f
KesseNones <jesse.jones@wsu.edu>
2023-02-07 16:48:15 -0800

    Added Some Skeleton Code to the ScoreBoard Class
    Some skeleton code of the scoreboard class and its methods
    has been added. Much is still needed though.

--- XtremePong/Source/XtremePong/Private/ScoreBoard.cpp
+++ XtremePong/Source/XtremePong/Private/ScoreBoard.cpp
@@ -4,13 +4,25 @@
 
 
 #include "ScoreBoard.h"
+#include <string>
+using namespace std;
 
 // Sets default values
 AScoreBoard::AScoreBoard()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+	
+	player1Score = 0;
+	player2Score = 0;
+	gameRound = 0;
 
+	roundWinCount = 10;
+	maxRoundCount = 3;
+
+	scoreModel = nullptr;
+	//Displays initial gamestate.
+	updateScoreboard();
 }
 
 // Called when the game starts or when spawned
@@ -27,3 +39,53 @@ void AScoreBoard::Tick(float DeltaTime)
 
 }
 
+void AScoreBoard::incrementPlayerScore(bool isPlayerOne) {
+	if (isPlayerOne) {
+		player1Score++;
+	}
+	else {
+		player2Score++;
+	}
+}
+
+void AScoreBoard::isWin() {
+	if (player1Score >= roundWinCount) {
+		//DISPLAY PLAYER 1 VICTORY
+		resetScoreToNewRound();
+	}
+
+	if (player2Score >= roundWinCount) {
+		//DISPLAY PLAYER 2 VICTORY
+		resetScoreToNewRound();
+	}
+}
+
+void AScoreBoard::resetScoreToNewRound() {
+	//Resets player scores and increments round.
+	player1Score = 0;
+	player2Score = 0;
+	gameRound++;
+
+	//Enters end state if max rounds reached.
+	if (gameRound > maxRoundCount) {
+		//END GAME LEVEL SOMEHOW.
+	}
+}
+
+void AScoreBoard::updateScoreboard() {
+	//Generates string representation of scoreboard.
+	scoreText = "Round: " + to_string(gameRound) + "\n"
+		+ "Player 1: " + to_string(player1Score) + " " +
+		"Player 2: " + to_string(player2Score);
+
+	//Spawns score representation 
+	// in the world if it doesn't exist yet.
+	if (scoreModel != nullptr) {
+		//DOCS: 
+		// https://docs.unrealengine.com/5.1/
+		// en-US/spawning-actors-in-unreal-engine/
+		// 
+		//USE SPAWNACTOR FUNCTION.
+		//USAGE: MyHUD = SpawnActor<AHUD>(this, Instigator);
+	}
+}
--- XtremePong/Source/XtremePong/Public/ScoreBoard.h
+++ XtremePong/Source/XtremePong/Public/ScoreBoard.h
@@ -1,5 +1,5 @@
 //Jesse A. Jones
-//4 Feb, 2023
+//7 Feb, 2023
 //XtremePong
 
 #pragma once
@@ -28,4 +28,24 @@ public:
 	virtual void Tick(float DeltaTime) override;
 
 	GENERATED_BODY()
+
+	int player1Score, player2Score,
+		gameRound, roundWinCount, maxRoundCount;
+	
+	std::string scoreText;
+	AActor* scoreModel;
+
+	//Increases the score of a given player by 1.
+	void incrementPlayerScore(bool isPlayerOne);
+		
+	//Checks if a given player has won a round/game.
+	void isWin(); //ADD PARTICLE GENERATION LATER!
+
+	//Resets player scores and increments to new round.
+	// If maxRoundCount rounds have occured, the game is over.
+	void resetScoreToNewRound();
+
+	//Updates the visual component of the scoreboard.
+	void updateScoreboard();
+
 };
