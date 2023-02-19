81449147ae7c736229b0e019fab8d708e21a6e25
KesseNones <jesse.jones@wsu.edu>
2023-02-19 15:29:36 -0800

    Added Ben's Ball to Jesse branch.
    The ball class was added to this branch to be used in scoreboard
    development. Ben made the ball code.

---
+++ XtremePong/Source/XtremePong/Private/Ball.cpp
@@ -0,0 +1,84 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Ball.h"
+
+
+// Sets default values
+ABall::ABall()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	
+	CollisionSphere = CreateDefaultSubobject<USphereComponent>(TEXT("CollisionSphere"));
+	//CollisionSphere->SetupAttachment(RootComponent);
+	RootComponent = CollisionSphere;
+
+	CollisionSphere->SetSimulatePhysics(true);
+	CollisionSphere->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+	CollisionSphere->SetSphereRadius(BallSize);
+	CollisionSphere->SetEnableGravity(false);
+
+	CollisionSphere->SetCollisionProfileName(TEXT("BlockAllDynamic"));
+
+	//Make the mesh texture
+	UStaticMeshComponent* texture = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	texture->SetupAttachment(CollisionSphere);
+
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> SphereMesh(TEXT("StaticMesh'/Engine/BasicShapes/Sphere.Sphere'"));
+	static ConstructorHelpers::FObjectFinder<UMaterial> SphereMaterial(TEXT("Material'/Engine/BasicShapes/BasicShapeMaterial.BasicShapeMaterial'"));
+
+	if (SphereMesh.Succeeded()) texture->SetStaticMesh(SphereMesh.Object);
+	if (SphereMaterial.Succeeded()) texture->SetMaterial(0, SphereMaterial.Object);
+
+	texture->SetWorldScale3D(FVector(0.2f));
+	
+}
+
+// Called when the game starts or when spawned
+void ABall::BeginPlay()
+{
+	Super::BeginPlay();
+
+	//This is the initial velocity
+	Velocity = FVector(Speed, Speed, 0.0f);
+
+}
+
+// Called every frame
+void ABall::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	/*
+		This section sets ball vector direction at a constant speed per tick
+	*/
+	
+	FVector NewLocation = GetActorLocation() + Velocity * DeltaTime;
+	//GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, FString::Printf(TEXT("%f"), DeltaTime));
+
+	SetActorLocation(NewLocation);
+
+	// Define the origin and direction of the line trace
+	FVector TraceStart = GetActorLocation();
+	FVector TraceEnd = TraceStart + Velocity * DeltaTime*5;
+
+	// Define the parameters of the line trace
+	FCollisionQueryParams TraceParams;
+	TraceParams.AddIgnoredActor(this);
+	// Perform the line trace
+	FHitResult TraceResult;
+	bool bDidHit = GetWorld()->LineTraceSingleByChannel(TraceResult, TraceStart, TraceEnd, ECC_Visibility, TraceParams);
+
+	FVector surface_normal = TraceResult.ImpactNormal;
+	FVector Reflection = UKismetMathLibrary::GetReflectionVector(Velocity, surface_normal);
+
+	//Check if the line trace hit an Actor
+	if (bDidHit) {
+
+		Reflection.Z = Velocity.Z; //Z doesn't change since we're in 2D only
+		Velocity = Reflection;
+	}
+		
+}
+
---
+++ XtremePong/Source/XtremePong/Public/Ball.h
@@ -0,0 +1,49 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Components/SphereComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "GameFramework/ProjectileMovementComponent.h"
+#include "Engine/EngineTypes.h"
+#include "Engine/StaticMesh.h"
+#include "Kismet/KismetMathLibrary.h"
+
+#include "Ball.generated.h"
+
+
+UCLASS()
+class XTREMEPONG_API ABall : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ABall();
+
+	virtual void Tick(float DeltaTime) override;
+
+	//ball component
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Mesh")
+	UStaticMeshComponent* ballMesh;
+
+	UPROPERTY(VisibleAnywhere, Category = "Collision")
+	USphereComponent* CollisionSphere;
+	UPROPERTY(EditAnywhere, Category = "BallSize")
+		float BallSize = 10.0f;
+	
+
+	UPROPERTY(EditAnywhere)
+		float BounceFactor = 1.2f;
+
+	FVector Velocity;
+	float Speed = 1000.0f;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	
+
+};
